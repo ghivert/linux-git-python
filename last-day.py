d27807ea44ced9a9352f11875a80cc1479807f2b
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-09-04 10:00:02 +0200

    :recycle: Improve code

--- last-day.py
+++ last-day.py
@@ -2,16 +2,21 @@ import requests
 import json
 
 def query_github():
+    # Si jamais pas de 403
     url = "enter-github-api-url"
     response = requests.get(url)
     data = response.json()
+
+    # Si jamais 403
+    path = './repositories.json' # chemin vers repositories.json ou repositories-stars.json pour la deuxième question
+    data = read_from_local_data(path)
+
     for dict in data:
         print(dict["name"])
         print(dict["description"])
 
 # Si jamais vous tombez sur une Erreur 403:
-def read_from_local_data():
-    path = './repositories.json' # chemin vers repositories.json ou repositories-stars.json pour la deuxième question
+def read_from_local_data(path):
     file = open(path)
     content = file.read()
     file.close()
